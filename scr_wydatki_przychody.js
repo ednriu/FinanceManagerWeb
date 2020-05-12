8227cd1f28e61556461ae0a76b787adbfc6a20ed
Andrzej Konicki <andrzej.konicki.programista@gmail.com>
2020-05-12 15:57:09 +0200

    BILANS improved, corrected JS Script for Expences

--- scr_wydatki_przychody.js
+++ scr_wydatki_przychody.js
@@ -1,28 +1,53 @@
 //Script fills the data input with today date as default
 
-var date = '';
-var today = new Date();
-
-date = today.getFullYear();
-
-if(today.getMonth().length=1)
+function TodayDate()
 {
-	date = date + '-'+ '0'+(today.getMonth()+1);
-}
-else
-{
-	date = date + '-'+(today.getMonth()+1);
+	var date = '';
+	var today = new Date();
+
+	date = today.getFullYear();
+
+	if(today.getMonth().toString().length==1)
+	{
+		date = date + '-'+ '0'+(today.getMonth()+1);
+	};
+	if(today.getMonth().toString().length==2)
+	{
+		date = date + '-'+(today.getMonth()+1);
+	};
+
+	if(today.getDate().toString().length==1)
+	{
+		date = date + '-0'+today.getDate();
+	};
+	if(today.getDate().toString().length==2)
+	{
+		date = date + '-' +today.getDate();
+	};
+	document.getElementById('datep').value = date;
+	alert("działaTodayDate");
 };
 
-if(today.getDate().length=1)
+function test()
 {
-	date = date + '-0'+today.getDate();
-}
-else
-{
-	date = date + '-' +today.getDate();
+	alert("test Alert");
 };
-document.getElementById('datep').value = date;
+
+function handleClick(myRadio) {
+	alert("działa1");
+	if(myRadio.value== "true")
+		{
+		document.getElementById("datep1").disabled = true;
+		document.getElementById("datep2").disabled = true;
+		}
+	else
+		{
+		document.getElementById("datep1").disabled = false;
+		document.getElementById("datep2").disabled = false;
+		}
+};
+
+
 
 
 
